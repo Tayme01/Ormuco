77ad5932c187c05a17965c6b3bf5db3a798178ad
Temi Oluseun <temi.oluseun@yahoo.com>
2019-04-27 14:05:42 +0100

    Added test assertions for greater than, less than and equal to

--- Question 2/lib/tests.py
+++ Question 2/lib/tests.py
@@ -5,11 +5,15 @@ from .number_comparator import VersionComparator
 class TestNumberComparator(unittest.TestCase):
     def test_greater_than(self):
         self.assertEqual(VersionComparator('1.1', '1.01').compare(), '"1.1” is greater than “1.01”.',
-            'Should be greater than')
+                         'Should be greater than')
 
     def test_less_than(self):
         self.assertEqual(VersionComparator('1.3', '1.4').compare(), '"1.3” is less than “1.4”.',
-            'Should be less than')
+                         'Should be less than')
+
+    def test_equal_to(self):
+        self.assertEqual(VersionComparator('1.5', '1.5').compare(), '"1.5” is equal to “1.5”.',
+                         'Should be equal to')
 
 
 if __name__ == '__main__':
