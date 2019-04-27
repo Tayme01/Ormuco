0e5375ba441bbd1c6855f2ff5f64ccad70f26c81
Temi Oluseun <temi.oluseun@yahoo.com>
2019-04-27 14:32:34 +0100

    Added docstring and useful comments

--- Question 2/lib/number_comparator.py
+++ Question 2/lib/number_comparator.py
@@ -1,7 +1,14 @@
 class VersionComparator:
-
+    """
+        Takes two strings, each representing a version number and compares both to determine if one is greater than the
+        other or both are equal.
+    """
     def __init__(self, first='nan', second='nan'):
+        # Ensure the right parameters are passed. If not, gracefully handle error.
         try:
+            if not isinstance(first, str) or not isinstance(second, str):
+                raise TypeError()
+
             self.first = float(first)
             self.second = float(second)
         except (TypeError, ValueError):
